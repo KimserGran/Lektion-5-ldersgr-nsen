f11dce442d1e4d26aba3936881b9096565422e72
kim granzell <kimzell_94@hotmail.se>
2026-09-21 12:36:06 +0200

    ny lektion

--- 15.Short-circuiting som logisk grindvakt/Program.cs
+++ 15.Short-circuiting som logisk grindvakt/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text1 = null;
+            if (text1 != null && text1.Length > 5)
+                Console.WriteLine("Not null");
+            else throw new NullReferenceException("Null Reference");
+            if (text1.Length > 5 && text1 != null)
+                Console.WriteLine("Not Null");
+            else throw new NullReferenceException("a");
+
         }
     }
 }
